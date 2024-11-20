b5e45f887fc533d709833a06aa7dae2c0df46fb6
Raphael Pitol <raphaelpitoljuliani@gmail.com>
2024-11-20 16:22:02 -0300

    Colocando icone de localização

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,19 +59,19 @@
             <div class="container">
                 @auth
                 @if (Auth::user()->permission == 'vistoriador')
-                <a class="navbar-brand d-flex align-items-center justify-content-center" href="{{route('vistoriador.home')}}">
+                <a class="navbar-brand d-flex align-items-center justify-content-center" href="{{route('vistoriador.home')}}" title="Voltar ao Inicio">
                 <img src="/image/casa.png" alt="" class="img-fluid align-middle" style="height: 1.2em; margin-right: 0.5em;">
                     {{ config('app.name', 'VistoriaPro') }}
                 </a>
                 @endif
                 @if (Auth::user()->permission == 'admin')
-                <a class="navbar-brand d-flex align-items-center justify-content-center" href="{{route('admin.home')}}">
+                <a class="navbar-brand d-flex align-items-center justify-content-center" href="{{route('admin.home')}}" title="Voltar ao Inicio">
                 <img src="/image/casa.png" alt="" class="img-fluid align-middle" style="height: 1.2em; margin-right: 0.5em;">
                     {{ config('app.name', 'VistoriaPro') }}
                 </a>
                 @endif
                 @if (Auth::user()->permission == 'imobiliaria')
-                <a class="navbar-brand d-flex align-items-center justify-content-center" href="{{ route('imobiliaria.home') }}">
+                <a class="navbar-brand d-flex align-items-center justify-content-center" href="{{ route('imobiliaria.home') }}" title="Voltar ao Inicio">
                     <img src="/image/casa.png" alt="" class="img-fluid align-middle" style="height: 1.2em; margin-right: 0.5em;">
                     {{ config('app.name', 'VistoriaPro') }}
                 </a>
